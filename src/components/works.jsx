9943217cf15c016bbd530294d00eafb4b7ffbcba
Joebim <josey359@gmail.com>
2023-01-12 14:35:38 +0100

    [ADD]Recent resume

--- src/components/works.jsx
+++ src/components/works.jsx
@@ -117,7 +117,7 @@ export default function Works(props) {
                 <div className={`work-info-contain justify-center items-center py-[50px] flex flex-col ${eval("toggleCard"+workDetails.id) ? "show" : ""}`}>
                     <h3 className="work-info-header font-['overlock'] pt-[100px] pb-[200px] text-9xl font-black text-center">{workDetails.name}</h3>
                  <div className="text-contain">
-                 <h4 className="work-info-details pt-[50px] pb-[150px] text-[85px] lg:text-[80px] leading-[120px] lg:leading-[100px] text-center">{workDetails.innerInfo}</h4>
+                 <h4 className="work-info-details pt-[50px] pb-[150px] text-[85px] lg:text-[70px] leading-[120px] lg:leading-[100px] text-center">{workDetails.innerInfo}</h4>
                  </div>
                 </div>
                  
@@ -138,7 +138,7 @@ export default function Works(props) {
         })}
 
         <AnimateButtonPoint animateButtonClass={"btn-end"}/>
-{console.log('worksData.length', worksData.length)}
+{/* {console.log('worksData.length', worksData.length)} */}
 
             {worksData.length <= 6 ? "" :<div className="h-[300px] w-full flex items-center flex-col-reverse">
                 
