23b36dc58409d30807939b6bc4b2dd54774956ee
Joebim <josey359@gmail.com>
2023-06-20 23:44:43 +0100

    [ADD] twitter handle link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,7 @@ import dribble from "../assets/images/dribbble.png"
 import instagram from "../assets/images/instagram.png"
 // import tumblr from "../assets/images/tumblr.png"
 import github from "../assets/images/github-dark.jpg"
+import twitter from "../assets/images/twitter.png"
 
 export default function Footer() {
 
@@ -16,8 +17,8 @@ export default function Footer() {
             <a href="https://www.linkedin.com/in/joseph-akinwole-71415a198/"><img src={linkedIn} alt="" height="130" id="linkedin-logo" className="h-40"></img></a>
             <a href="https://dribbble.com/Joebim"><img src={dribble} alt="" height="130" id="dribble-logo" className="h-40"></img></a>
             <a href="https://github.com/Joebim"><img src={github} alt="" height="130" id="github-logo" className="rounded-[120px] h-40"></img></a>
-            {/* <a href=""><img src={tumblr} alt="" height="130" id="tumblr-logo"></img></a> */}
             <a href="https://www.instagram.com/josephakinwole/"><img src={instagram} alt="" height="130" id="instagram-logo" className="h-40"></img></a>
+            <a href="https://twitter.com/akinwole_joseph"><img src={twitter} alt="" height="130" id="twitter-logo" className="h-40"></img></a>
         </div>
         
         
